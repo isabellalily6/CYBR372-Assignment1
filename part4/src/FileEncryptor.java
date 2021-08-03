d01a01a889f6147b5587fa6e9e4c0050255236d4
Isabella <isabellaketley@gmail.com>
2021-08-03 23:57:39 +1200

    Reformatted part 4 code
    Added comments and rearranged the code in part 4 so it is easy to read and understand

--- part4/src/FileEncryptor.java
+++ part4/src/FileEncryptor.java
@@ -9,19 +9,16 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
-import java.util.ArrayList;
 import java.util.Base64;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.PBEParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
- * @author Erik Costlow
+ * @author Isabella Tomaz-Ketley
  */
 public class FileEncryptor {
     private static final Logger LOG = Logger.getLogger(FileEncryptor.class.getSimpleName());
@@ -29,193 +26,216 @@ public class FileEncryptor {
     private static String CIPHER = "/CBC/PKCS5PADDING";
     private static int COUNT = 1000;
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IOException, InvalidKeySpecException {
-        if ((args.length != 6 && args.length != 4 && args.length != 2)
-                && (!args[0].equals("enc") && !args[0].equals("dec") && args[0].equals("info"))) {
-            System.out.println("Wrong arguments given");
+    /**
+     * Main method which checks whether the arguments passed in are correct
+     * and if so calls, the corresponding methods.
+     *
+     * @param args the terminal arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // Check whether any arguments are given
+        if ((args.length <= 0)) {
+            System.out.println("No arguments given");
             return;
         }
 
         String state = args[0];
 
+        // Check whether the correct number of arguments are given, depending on the action
+        if ((args.length == 6 && !state.equals("enc")) || (args.length == 4 && !state.equals("dec"))
+                || (args.length == 2 && !state.equals("info"))) {
+            System.out.println("Wrong arguments given");
+            return;
+        }
+
         if (state.equals("enc")) {
-            CIPHER = args[1] + CIPHER;
-            int keyLength = Integer.parseInt(args[2]);
+            // if the state is enc, set the cipher and encrypt the file
             String cipher = args[1];
-            String password = args[3];
-            String inputFile = args[4];
-            String outputFile = args[5];
-            System.out.println(CIPHER);
-            encryption(cipher, keyLength, password, inputFile, outputFile);
+            CIPHER = cipher + CIPHER;
+            int keyLength = Integer.parseInt(args[2]);
+            encryption(cipher, keyLength, args[3], args[4], args[5]);
         } else if (state.equals("dec")) {
-            String password = args[1];
-            String inputFile = args[2];
-            String outputFile = args[3];
-            decryption(password, inputFile, outputFile);
+            // if the state is dec, decrypt the file
+            decryption(args[1], args[2], args[3]);
         } else if (state.equals("info")) {
-            System.out.println(getMetaData(args[1]));
+            // if the state is info, show the correct info of the file
+            System.out.println(getMetaData(args[1], null));
         }
     }
 
-    public static void encryption(String cipher, Integer keyLength, String password, String inputFile, String outputFile) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IOException, InvalidKeySpecException {
-        //This snippet is literally copied from SymmetrixExample
+    /**
+     * Encrypts a given file, to an output file using the information passed as parameters.
+     *
+     * @param cipher the cipher algorithm
+     * @param keyLength the length of the key
+     * @param password the password used to encrypt the file
+     * @param inputFile the input file to encrypt
+     * @param outputFile the output file of the encrypted information
+     */
+    public static void encryption(String cipher, Integer keyLength, String password, String inputFile, String outputFile) {
         SecureRandom sr = new SecureRandom();
-
-        PBEKeySpec pbeKeySpec;
-        PBEParameterSpec pbeParamSpec;
-        SecretKeyFactory keyFac;
-
+        // create the 16 byte salt
         byte[] salt = new byte[16];
-        sr.nextBytes(salt); // 16 bytes salt
+        sr.nextBytes(salt);
+        // create the 16 or 8 bytes IV depending on algorithm
         int ivLength = cipher.equals("AES") ? 16 : 8;
         byte[] iv = new byte[ivLength];
-        sr.nextBytes(iv); // 16 bytes salt
+        sr.nextBytes(iv);
         IvParameterSpec ivParamSpec = new IvParameterSpec(iv);
 
-        // Iteration count
-        int count = 1000;
-
-        // Create PBE parameter set
-        pbeParamSpec = new PBEParameterSpec(salt, count, ivParamSpec);
-        char[] passwordChar = password.toCharArray();
-        //pbeKeySpec = new PBEKeySpec(passwordChar, salt, count, keyLength);
-       // keyFac = SecretKeyFactory.getInstance("PBEWithHmacSHA256AndAES_256");
-        //SecretKey pbeKey = keyFac.generateSecret(pbeKeySpec);
-
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 1000, keyLength);
-        SecretKey pbeKey = new SecretKeySpec(factory.generateSecret(spec)
-                .getEncoded(), cipher);
-
-
-        // Create PBE Cipher
-        Cipher pbeCipher = Cipher.getInstance(CIPHER);
-        // Initialize PBE Cipher with key and parameters
-        pbeCipher.init(Cipher.ENCRYPT_MODE, pbeKey, ivParamSpec);
-
-        System.out.println("Random key=" + Base64.getEncoder().encodeToString(pbeKey.toString().getBytes()));
-        System.out.println("initVector=" + Base64.getEncoder().encodeToString(salt));
-
-        //Look for files here
+        // Determine where to find the files and find them
         final Path tempDir = Paths.get("").toAbsolutePath();
-
         final Path encryptedPath = tempDir.resolve(outputFile);
         final Path inputPath = tempDir.resolve(inputFile);
 
+        // try and open the input and output file
         try (InputStream fin = Files.newInputStream(inputPath);
-             OutputStream fout = Files.newOutputStream(encryptedPath);
-             CipherOutputStream cipherOut = new CipherOutputStream(fout, pbeCipher) {
-             }) {
-            final byte[] bytes = new byte[1024];
+             OutputStream fout = Files.newOutputStream(encryptedPath)) {
+
+            // Create the secret key
+            SecretKey pbeKey = generateSecretKey(password, salt, keyLength, cipher);
+
+            // Create PBE Cipher
+            Cipher pbeCipher = Cipher.getInstance(CIPHER);
+
+            // Initialize PBE Cipher with key and parameters
+            pbeCipher.init(Cipher.ENCRYPT_MODE, pbeKey, ivParamSpec);
+
+            writeMetaData(fout, cipher, keyLength, salt, iv);
+
+            // write the encrypted data to the output file
+            try (CipherOutputStream cipherOut = new CipherOutputStream(fout, pbeCipher)) {
+                final byte[] bytes = new byte[1024];
+                for (int length = fin.read(bytes); length != -1; length = fin.read(bytes)) {
+                    cipherOut.write(bytes, 0, length);
+                }
+            }
+            LOG.info("Encryption finished, saved at " + encryptedPath);
+        } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
+            LOG.log(Level.INFO, "Unable to encrypt", e);
+        }
+    }
+
+    /**
+     * Decrypts a given file, to an output file using the information passed as parameters.
+     * 
+     * @param password the password used to encrypt the file
+     * @param inputFile the input file to encrypt
+     * @param outputFile the output file of the encrypted information
+     */
+    public static void decryption(String password, String inputFile, String outputFile) {
+        // Determine where to find the files and find them
+        final Path tempDir = Paths.get("").toAbsolutePath();
+        final Path encryptedPath = tempDir.resolve(inputFile);
+        final Path decryptedPath = tempDir.resolve(outputFile);
+
+        // try and open the input and output file
+        try (InputStream encryptedData = Files.newInputStream(encryptedPath);
+             OutputStream decryptedOut = Files.newOutputStream(decryptedPath)) {
+            // Retrieve the cipher algorithm and key length
+            String[] cipherData = getMetaData(null, encryptedData).split(" ");
+            String cipher = cipherData[0];
+            int keyLength = Integer.parseInt(cipherData[1]);
+            CIPHER = cipher + CIPHER;
+
+            // Retrieve the salt and IV
+            byte[] salt = encryptedData.readNBytes(16);
+            int ivLength = cipher.equals("AES") ? 16 : 8;
+            byte[] initVector = encryptedData.readNBytes(ivLength);
+            IvParameterSpec ivParamSpec = new IvParameterSpec(initVector);
+
+            // Create the secret key
+            SecretKey pbeKey = generateSecretKey(password, salt, keyLength, cipher);
+
+            // Create PBE Cipher
+            Cipher pbeCipher = Cipher.getInstance(CIPHER);
+
+            // Initialize PBE Cipher with key and parameters
+            pbeCipher.init(Cipher.DECRYPT_MODE, pbeKey, ivParamSpec);
+
+            // decrypt the information and write it to the output file
+            try (CipherInputStream decryptStream = new CipherInputStream(encryptedData, pbeCipher)) {
+                final byte[] bytes = new byte[1024];
+                for (int length = decryptStream.read(bytes); length != -1; length = decryptStream.read(bytes)) {
+                    decryptedOut.write(bytes, 0, length);
+                }
+            }
+            LOG.info("Decryption complete, open " + decryptedPath);
+        } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException ex) {
+            Logger.getLogger(FileEncryptor.class.getName()).log(Level.SEVERE, "Unable to decrypt", ex);
+        }
+    }
+
+    /**
+     * Returns the metadata of a file which contains the cipher algorithm and key length.
+     *
+     * @param filename the input file to retrieve the information from.
+     *                 This is null if the input stream is given
+     * @param encryptedData the stream of corresponding to the file to read.
+     *                      This is null is no input stream has been opened yet
+     *
+     * @return a string containing the retrieved metadata
+     */
+    public static String getMetaData(String filename, InputStream encryptedData) throws IOException {
+        if (encryptedData == null) {
+            final Path tempDir = Paths.get("").toAbsolutePath();
+            final Path encryptedPath = tempDir.resolve(filename);
+            encryptedData = Files.newInputStream(encryptedPath);
+        }
+
+        byte[] cipherLength = encryptedData.readNBytes(1);
+        byte[] cipher = encryptedData.readNBytes(cipherLength[0]);
+        String cipherString = new String(cipher, StandardCharsets.UTF_8);
+
+        byte[] keyLen = encryptedData.readNBytes(1);
+        byte[] key = encryptedData.readNBytes(keyLen[0]);
+        String keyLengthString = new String(key, StandardCharsets.UTF_8);
+
+        return (cipherString + " " + keyLengthString);
+    }
+
+    /**
+     * Writes the metadata, which is the cipher algorithm and key length, to a given file.
+     * This method also writes the salt and IV to the file
+     *
+     * @param fout the file stream to write to
+     * @param cipher the cipher to write to the file
+     * @param keyLength the key length to write to the file
+     * @param salt the salt to write to the file
+     * @param iv the iv to write to the file
+     */
+    public static void writeMetaData(OutputStream fout, String cipher, Integer keyLength, byte[] salt, byte[] iv) {
+        try {
             fout.write(cipher.getBytes().length);
             fout.write(cipher.getBytes());
             fout.write(keyLength.toString().getBytes().length);
             fout.write(keyLength.toString().getBytes());
             fout.write(salt);
             fout.write(iv);
-            for (int length = fin.read(bytes); length != -1; length = fin.read(bytes)) {
-                cipherOut.write(bytes, 0, length);
-            }
         } catch (IOException e) {
             LOG.log(Level.INFO, "Unable to encrypt", e);
         }
-        LOG.info("Encryption finished, saved at " + encryptedPath);
-        writeMetaData(keyLength, outputFile);
-
     }
 
-    public static void decryption(String base64SecretKey, String inputFile, String outputFile) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IOException, InvalidKeySpecException {
-
-        String info = getMetaData(inputFile);
-        String[] data = info.split(" ");
-        CIPHER = data[0] + CIPHER;
-        int keyLength = Integer.parseInt(data[1]);
-
-        PBEKeySpec pbeKeySpec;
-        PBEParameterSpec pbeParamSpec;
-        SecretKeyFactory keyFac;
-
-        // Iteration count
-        int count = 1000;
-
-        // Create PBE parameter set
-        char[] passwordChar = base64SecretKey.toCharArray();
-
-
-        //Look for files here
-        final Path tempDir = Paths.get("").toAbsolutePath();
-        final Path encryptedPath = tempDir.resolve(inputFile);
-
-        final Path decryptedPath = tempDir.resolve(outputFile);
-        InputStream encryptedData = Files.newInputStream(encryptedPath);
-
-
+    /**
+     * Generates a secret key from a given password, salt, key length and cipher.
+     *
+     * @param salt the salt to use to create the key
+     * @param keyLength the key length to use to create the key
+     * @param cipher the cipher to use to create the key
+     *
+     * @return the secret key
+     */
+    public static SecretKey generateSecretKey(String password, byte[] salt, int keyLength, String cipher) {
+        SecretKey pbeKey = null;
+        System.out.println("password=" + Base64.getEncoder().encodeToString(pbeKey.toString().getBytes()));
         try {
-            byte[] cipherLength = encryptedData.readNBytes(1);
-            byte[] cipher = encryptedData.readNBytes(cipherLength[0]);
-            String s = new String(cipher, StandardCharsets.UTF_8);
-
-            byte[] keyLen = encryptedData.readNBytes(1);
-            byte[] key = encryptedData.readNBytes(keyLen[0]);
-            String ss = new String(key, StandardCharsets.UTF_8);
-            byte[] salt = encryptedData.readNBytes(16);
-            int ivLength = cipher.equals("AES") ? 16 : 8;
-            byte[] initVector = encryptedData.readNBytes(ivLength);
-
-
-
-            pbeKeySpec = new PBEKeySpec(passwordChar, salt, count, keyLength);
-            keyFac = SecretKeyFactory.getInstance("PBEWithHmacSHA256AndAES_256");
-
             SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-            KeySpec spec = new PBEKeySpec(passwordChar, salt, 1000, keyLength);
-            SecretKey pbeKey = new SecretKeySpec(factory.generateSecret(spec)
-                    .getEncoded(), s);
-
-            // Create PBE Cipher
-            Cipher pbeCipher = Cipher.getInstance(CIPHER);
-
-            IvParameterSpec ivParamSpec = new IvParameterSpec(initVector);
-
-            System.out.println("initVector=" + Base64.getEncoder().encodeToString(initVector));
-            System.out.println("password=" + Base64.getEncoder().encodeToString(pbeKey.toString().getBytes()));
-
-            pbeParamSpec = new PBEParameterSpec(salt, count, ivParamSpec);
-            // Initialize PBE Cipher with key and parameters
-            pbeCipher.init(Cipher.DECRYPT_MODE, pbeKey, ivParamSpec);
-
-
-            CipherInputStream decryptStream = new CipherInputStream(encryptedData, pbeCipher);
-            OutputStream decryptedOut = Files.newOutputStream(decryptedPath);
-            final byte[] bytes = new byte[1024];
-            for (int length = decryptStream.read(bytes); length != -1; length = decryptStream.read(bytes)) {
-                decryptedOut.write(bytes, 0, length);
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(FileEncryptor.class.getName()).log(Level.SEVERE, "Unable to decrypt", ex);
-        } finally {
-            encryptedData.close();
+            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, COUNT, keyLength);
+            pbeKey = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), cipher);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            LOG.log(Level.INFO, "Unable to encrypt", e);
         }
-
-        LOG.info("Decryption complete, open " + decryptedPath);
-    }
-
-    public static String getMetaData(String filename) throws IOException {
-        final Path tempDir = Paths.get("").toAbsolutePath();
-        final Path encryptedPath = tempDir.resolve(filename);
-        InputStream encryptedData = Files.newInputStream(encryptedPath);
-        byte[] cipherLength = encryptedData.readNBytes(1);
-        byte[] cipher = encryptedData.readNBytes(cipherLength[0]);
-        String s = new String(cipher, StandardCharsets.UTF_8);
-
-        byte[] keyLen = encryptedData.readNBytes(1);
-        byte[] key = encryptedData.readNBytes(keyLen[0]);
-        String ss = new String(key, StandardCharsets.UTF_8);
-
-        return (s + " " + ss);
-    }
-
-    public static void writeMetaData(int keylength, String filename) throws IOException {
-
+        return pbeKey;
     }
 }
