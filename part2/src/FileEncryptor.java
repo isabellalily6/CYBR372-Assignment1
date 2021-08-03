f0826199375d010518c168abd9eea5a39fb84a0c
Isabella <isabellaketley@gmail.com>
2021-08-04 10:57:33 +1200

    Reformatted and commented part 2 code

--- part2/src/FileEncryptor.java
+++ part2/src/FileEncryptor.java
@@ -27,7 +27,14 @@ public class FileEncryptor {
     private static final String ALGORITHM = "AES";
     private static final String CIPHER = "AES/CBC/PKCS5PADDING";
 
+    /**
+     * Main method which checks whether the arguments passed in are correct
+     * and if so calls, the corresponding methods.
+     *
+     * @param args the terminal arguments
+     */
     public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IOException {
+        // Check whether state and the correct number of arguments are given
         if (args.length != 4 || (!args[0].equals("enc") && !args[0].equals("dec"))) {
             System.out.println("Wrong arguments given");
             return;
@@ -36,13 +43,13 @@ public class FileEncryptor {
         String state = args[0];
 
         if (state.equals("enc")) {
+            // if the state is enc, set the cipher and encrypt the file
             String base64SecretKey = args[1];
             String inputFile = args[2];
             String outputFile = args[3];
             encryption(base64SecretKey, inputFile, outputFile);
-
-
         } else if (state.equals("dec")) {
+            // if the state is dec, decrypt the file
             String base64SecretKey = args[1];
             String inputFile = args[2];
             String outputFile = args[3];
@@ -50,36 +57,42 @@ public class FileEncryptor {
         }
     }
 
-    public static void encryption(String base64SecretKey, String inputFile, String outputFile) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IOException {
-        //This snippet is literally copied from SymmetrixExample
+    /**
+     * Encrypts a given file, to an output file using the key passed as a parameter.
+     *
+     * @param base64SecretKey the password used to encrypt the file, encoded in base64
+     * @param inputFile the input file to encrypt
+     * @param outputFile the output file of the encrypted information
+     */
+    public static void encryption(String base64SecretKey, String inputFile, String outputFile) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
         SecureRandom sr = new SecureRandom();
 
+        // Retrieve the key
         byte[] key = Base64.getDecoder().decode(base64SecretKey);
+        // create the 16 byte IV
         byte[] initVector = new byte[16];
-        sr.nextBytes(initVector); // 16 bytes IV
-
-        System.out.println("Random key=" + Base64.getEncoder().encodeToString(key));
-        System.out.println("initVector=" + Base64.getEncoder().encodeToString(initVector));
-        System.out.println(initVector);
-
+        sr.nextBytes(initVector);
         IvParameterSpec iv = new IvParameterSpec(initVector);
+
+        // Create and initialise the cipher using the key and IV
         SecretKeySpec skeySpec = new SecretKeySpec(key, ALGORITHM);
         Cipher cipher = Cipher.getInstance(CIPHER);
-
         cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
 
-        //Look for files here
+        // Determine where to find the files and find them
         final Path tempDir = Paths.get("").toAbsolutePath();
-
         final Path encryptedPath = tempDir.resolve(outputFile);
         final Path inputPath = tempDir.resolve(inputFile);
 
+        // open each of the files
         try (InputStream fin = Files.newInputStream(inputPath);
              OutputStream fout = Files.newOutputStream(encryptedPath);
              CipherOutputStream cipherOut = new CipherOutputStream(fout, cipher) {
              }) {
             final byte[] bytes = new byte[1024];
+            // write the IV to the start of the file
             fout.write(initVector);
+            // encrypt and write the encrypted data to the output file
             for (int length = fin.read(bytes); length != -1; length = fin.read(bytes)) {
                 cipherOut.write(bytes, 0, length);
             }
@@ -90,39 +103,45 @@ public class FileEncryptor {
 
     }
 
-    public static void decryption(String base64SecretKey, String inputFile, String outputFile) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IOException {
+    /**
+     * Decrypts a given file, to an output file using the information passed as parameters.
+     *
+     * @param base64SecretKey the password used to encrypt the file, encoded in base64
+     * @param inputFile the input file to encrypt
+     * @param outputFile the output file of the encrypted information
+     */
+    public static void decryption(String base64SecretKey, String inputFile, String outputFile) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
+        // Retrieve the key
         byte[] key = Base64.getDecoder().decode(base64SecretKey);
 
-        //Look for files here
+        // Determine where to find the files and find them
         final Path tempDir = Paths.get("").toAbsolutePath();
         final Path encryptedPath = tempDir.resolve(inputFile);
-
         final Path decryptedPath = tempDir.resolve(outputFile);
-        InputStream encryptedData = Files.newInputStream(encryptedPath);
 
-
-        try {
+        // open the encrypted file
+        try (InputStream encryptedData = Files.newInputStream(encryptedPath)) {
+            // read and create the IV from the data in the file
             byte[] initVector = encryptedData.readNBytes(16);
-
-            System.out.println("initVector=" + Base64.getEncoder().encodeToString(initVector));
-
             IvParameterSpec iv = new IvParameterSpec(initVector);
+
+            // Create and initialise the cipher using the key and IV
             SecretKeySpec skeySpec = new SecretKeySpec(key, ALGORITHM);
             Cipher cipher = Cipher.getInstance(CIPHER);
             cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
 
-            CipherInputStream decryptStream = new CipherInputStream(encryptedData, cipher);
-            OutputStream decryptedOut = Files.newOutputStream(decryptedPath);
-            final byte[] bytes = new byte[1024];
-            for (int length = decryptStream.read(bytes); length != -1; length = decryptStream.read(bytes)) {
-                decryptedOut.write(bytes, 0, length);
+            // open the files and streams to decrypt the data and write it to the output file
+            try (CipherInputStream decryptStream = new CipherInputStream(encryptedData, cipher);
+            OutputStream decryptedOut = Files.newOutputStream(decryptedPath)) {
+                final byte[] bytes = new byte[1024];
+                // decrypt and write the decrypted data to the output file
+                for (int length = decryptStream.read(bytes); length != -1; length = decryptStream.read(bytes)) {
+                    decryptedOut.write(bytes, 0, length);
+                }
             }
         } catch (IOException ex) {
             Logger.getLogger(FileEncryptor.class.getName()).log(Level.SEVERE, "Unable to decrypt", ex);
-        } finally {
-            encryptedData.close();
         }
-
         LOG.info("Decryption complete, open " + decryptedPath);
     }
 }
